7278ff17a02c423794f7aa886923a3e4fadfd5e8
l.brodowski <lukasz.brodowski@external.nexigroup.com>
2024-01-29 17:20:54 +0100

    fix: reformatting translate files

--- Application/views/admin_twig/de/nexi-checkout_admin_lang.php
+++ Application/views/admin_twig/de/nexi-checkout_admin_lang.php
@@ -2,6 +2,10 @@
 
 $aLang = [
     'charset' => 'UTF-8',
+    'nexi_checkout' => 'Nexi Checkout',
+
+    // Nexi Checkout Settings
+    'SHOP_MODULE_GROUP_nexi_checkout_main' => 'Nexi Checkout Einstellungen',
     'SHOP_MODULE_nexi_checkout_merchant_id' => 'Händler ID',
     'SHOP_MODULE_nexi_checkout_blMode' => 'Modus',
     'SHOP_MODULE_nexi_checkout_blMode_0' => 'Test',
@@ -17,18 +21,24 @@ $aLang = [
     'SHOP_MODULE_nexi_checkout_merchant_terms_url' => 'Nutzungsbedingungs-Url',
     'SHOP_MODULE_nexi_checkout_autocapture' => 'Automatische Abbuchung',
     'SHOP_MODULE_nexi_checkout_payment_text' => 'Zahlungsmethode Text',
-    'SHOP_MODULE_GROUP_nexi_checkout_main' => 'Nexi Checkout Einstellungen',
+
+    // Order Messages
     'SHOP_MODULE_nexi_checkout_chargeall' => 'BELASTUNG ALLE',
     'SHOP_MODULE_nexi_checkout_refundall' => 'RÜCKERSTATTUNG ALLE',
     'SHOP_MODULE_nexi_checkout_refund_msg' => 'Bestellung wird zurückerstattet',
     'SHOP_MODULE_nexi_checkout_cancel_msg' => 'Bestellung wird storniert',
     'SHOP_MODULE_nexi_checkout_refund_pending' => 'Rückerstattung steht noch aus',
     'SHOP_MODULE_nexi_checkout_failed_msg' => 'Bestellung ist fehlgeschlagen',
+    'SHOP_MODULE_nexi_checkout_payment_failed_msg' => 'Bestellung fehlgeschlagen. Keine Zahlung erhalten',
+
+    // Order Actions
     'SHOP_MODULE_nexi_checkout_cancel' => 'Stornieren',
     'SHOP_MODULE_nexi_checkout_reserve' => 'Reservieren',
     'SHOP_MODULE_nexi_checkout_charge' => 'Belasten',
     'SHOP_MODULE_nexi_checkout_refund' => 'Rückerstatten',
     'SHOP_MODULE_nexi_checkout_fail' => 'Fehlgeschlagen',
+
+    // Payment Status
     'SHOP_MODULE_nexi_checkout_payment_id' => 'Zahlungskennung',
     'SHOP_MODULE_nexi_checkout_paystatus_reserved' => 'Reserviert',
     'SHOP_MODULE_nexi_checkout_paystatus_charged' => 'Belastet',
@@ -38,10 +48,10 @@ $aLang = [
     'SHOP_MODULE_nexi_checkout_paystatus_partial_refund' => 'Teiweise rückerstattet',
     'SHOP_MODULE_nexi_checkout_paystatus_cancel' => 'Storniert',
     'SHOP_MODULE_nexi_checkout_paystatus_refunded' => 'Rückerstattet',
-    'SHOP_MODULE_nexi_checkout_payment_failed_msg' => 'Bestellung fehlgeschlagen. Keine Zahlung erhalten',
     'SHOP_MODULE_nexi_checkout_status' => 'Status',
+
+    // Debug Mode
     'SHOP_MODULE_nexi_checkout_blDebug_log' => 'Debug Modus',
-    'HELP_SHOP_MODULE_nexi_checkout_blDebug_log' => 'Zeigt im Übersichts Tab einer Bestellung die Bestellinfos von der Nexi Checkout API an.',
-    'nexi_checkout' => 'Nexi Checkout'
+    'HELP_SHOP_MODULE_nexi_checkout_blDebug_log' => 'Zeigt im Übersichts Tab einer Bestellung die Bestellinfos von der Nexi Checkout API an.'
 ];
 
--- Application/views/admin_twig/en/nexi-checkout_admin_lang.php
+++ Application/views/admin_twig/en/nexi-checkout_admin_lang.php
@@ -2,6 +2,10 @@
 
 $aLang = [
     'charset' => 'UTF-8',
+    'nexi_checkout' => 'Nexi Checkout',
+
+    // Nexi Checkout Settings
+    'SHOP_MODULE_GROUP_nexi_checkout_main' => 'Nexi Checkout settings',
     'SHOP_MODULE_nexi_checkout_merchant_id' => 'Merchant ID',
     'SHOP_MODULE_nexi_checkout_blMode' => 'Mode',
     'SHOP_MODULE_nexi_checkout_blMode_0' => 'Test',
@@ -17,18 +21,24 @@ $aLang = [
     'SHOP_MODULE_nexi_checkout_merchant_terms_url' => 'Terms Url',
     'SHOP_MODULE_nexi_checkout_autocapture' => 'Auto Capture',
     'SHOP_MODULE_nexi_checkout_payment_text' => 'Payment method text',
-    'SHOP_MODULE_GROUP_nexi_checkout_main' => 'Nexi Checkout settings',
+
+    // Order Messages
     'SHOP_MODULE_nexi_checkout_chargeall' => 'CHARGE ALL',
     'SHOP_MODULE_nexi_checkout_refundall' => 'REFUND ALL',
     'SHOP_MODULE_nexi_checkout_refund_msg' => 'Order is Refunded',
     'SHOP_MODULE_nexi_checkout_cancel_msg' => 'Order is Cancelled',
     'SHOP_MODULE_nexi_checkout_refund_pending' => 'Refund is pending',
     'SHOP_MODULE_nexi_checkout_failed_msg' => 'Order is Failed',
+    'SHOP_MODULE_nexi_checkout_payment_failed_msg' => 'Order Failed. No payment recieved',
+
+    // Order Actions
     'SHOP_MODULE_nexi_checkout_cancel' => 'Cancel',
     'SHOP_MODULE_nexi_checkout_reserve' => 'Reserve',
     'SHOP_MODULE_nexi_checkout_charge' => 'Charge',
     'SHOP_MODULE_nexi_checkout_refund' => 'Refund',
     'SHOP_MODULE_nexi_checkout_fail' => 'Fail',
+
+    // Payment Status
     'SHOP_MODULE_nexi_checkout_payment_id' => 'Payment ID',
     'SHOP_MODULE_nexi_checkout_paystatus_reserved' => 'Reserved',
     'SHOP_MODULE_nexi_checkout_paystatus_charged' => 'Charged',
@@ -38,10 +48,10 @@ $aLang = [
     'SHOP_MODULE_nexi_checkout_paystatus_partial_charge' => 'Partial Charged',
     'SHOP_MODULE_nexi_checkout_paystatus_partial_refund' => 'Partial Refunded',
     'SHOP_MODULE_nexi_checkout_paystatus_cancel' => 'Cancelled',
-    'SHOP_MODULE_nexi_checkout_payment_failed_msg' => 'Order Failed. No payment recieved',
     'SHOP_MODULE_nexi_checkout_status' => 'Status',
+
+    // Debug Mode
     'SHOP_MODULE_nexi_checkout_blDebug_log' => 'Debug mode',
     'HELP_SHOP_MODULE_nexi_checkout_blDebug_log' => 'Show the order info from the Nexi Checkout API in the overview tab of the order.',
-    'nexi_checkout' => 'Nexi Checkout'
 ];
 
